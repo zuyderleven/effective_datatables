54e7cb8494ca4716616c69236fff1f799634c2d6
Joris <jj.martens@zuyderleven.nl>
2018-07-13 13:04:58 +0200

    Changed order of boostrap imports

--- app/assets/javascripts/effective_datatables.js
+++ app/assets/javascripts/effective_datatables.js
@@ -2,21 +2,23 @@
 //= require vendor/jquery.fileDownload
 
 //= require dataTables/jquery.dataTables
-//= require dataTables/dataTables.bootstrap
+
 
 //= require dataTables/buttons/dataTables.buttons
 //= require dataTables/buttons/buttons.bootstrap
 //= require dataTables/buttons/buttons.colVis
 //= require dataTables/buttons/buttons.html5
 //= require dataTables/buttons/buttons.print
-//= require dataTables/responsive/dataTables.responsive
-//= require dataTables/responsive/responsive.bootstrap
+//= require dataTables/dataTables.bootstrap
+
 
 //= require effective_datatables/bulk_actions
 //= require effective_datatables/events
 //= require effective_datatables/filters
 //= require effective_datatables/reset
 //= require effective_datatables/responsive
+//= require dataTables/responsive/dataTables.responsive
+//= require dataTables/responsive/responsive.bootstrap
 
 //= require effective_datatables/charts
 //= require effective_datatables/initialize
